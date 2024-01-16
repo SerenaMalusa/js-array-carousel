108e598e4f651f89e22fa9a3dcbf2025c6a3e17d
SerenaMalusa <sere.malu@gmail.com>
2024-01-16 19:43:46 +0100

    feat: adjust code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 // prendo l'elemento html wrapper dove andrò ad inserire le slides
 const slidesEl = document.querySelector('.slides');
-
-// 1. creo array che contiene i nomi delle immagini che devo inserire
-const pictures = ['1','2','3','4','5'];
-
 // creo variabile che conterrà la stringa html da inserire in pagina
 let slidesHtml = '';
 
+// 1. creo array che contiene i nomi delle immagini che devo inserire
+const pictures = ['1','2','3','4','5'];
 // creo la variabile che determina la slide con focus
 let slideFocus = pictures.length - 1;
+
 
 // 2. creo ciclo che conta da 1 a lunghezza dell'array
 for (i=0; i<pictures.length; i++) {
@@ -16,10 +15,8 @@ for (i=0; i<pictures.length; i++) {
     // mi prendo il singolo elemento il cui indice corrisponda all'iterazione del ciclo
     let picture = pictures[i];
 
-    // creo la variabile displayClass
+    // creo la variabile displayClass che è vuota solo per la slide con focus
     let displayClass = 'd-none';
-
-    // displayClass è vuota solo per la slide con focus
     if (i == slideFocus) displayClass = '';
 
     // 3. per ogni ciclo devo creare la stringa
